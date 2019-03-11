db6663b271840274ed9a4bc553cc775c2ce6e76e
Shantell Moore <moores1@hawkmail.newpaltz.edu>
2019-03-11 11:14:12 -0400

    change jackage.json

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,24 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="row">
+    <div class="col">
+      <h1>This is {{name}}'s page. She is {{age}} years old</h1>
+      <input v-model="age" type="text" class="form-control" placeholder="Age"/>
+      <div class="progress">
+       <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" :style="{width: age + '%'}"></div>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    name: "Shan",
+    age: "23"
+  })
+}
+</script>
+
+<style type="scss">
+
+</style>
