570a5312f1104e8953da22c5c2b7a3ca4b03e446
Shantell Moore <moores1@hawkmail.newpaltz.edu>
2019-03-15 22:26:02 -0400

    in class working

---
+++ src/views/Myfriends.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+    <h1> My friends Page </h1>
+    <ul>
+        <li> Friend 1</li>
+        <li> Friend 1</li>
+        <li> Friend 1</li>
+        <li> Friend 1</li>
+        <li> Friend 1</li>
+    </ul>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
