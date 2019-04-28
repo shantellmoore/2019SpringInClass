1ab71558fad2e78cb496c1ec4d1a05b07a25efa2
Shantell Moore <moores1@hawkmail.newpaltz.edu>
2019-04-28 16:01:33 -0400

    new change

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express   = require('express');
 const path      = require('path');
 const users     = require('./controllers/users');
-const userModel     = require('./models/user');
+const userModel  = require('./models/user');
 
 const app = express();
 const port = 3000;
